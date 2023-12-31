860a266128f65a0c9b2042b5613795cb473dd049
Tanvi Jain <tanvijain1511@gmail.com>
2023-12-31 19:16:22 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import Quiz from "./Pages/Quiz/Quiz";
+import Result from "./Pages/Result/Result";
 
 function App() {
+  const [name, setName] = useState("");
+  const [questions, setQuestions] = useState();
+  const [score, setScore] = useState(0);
+
+  const fetchQuestions = async (category = "", difficulty = "") => {
+    try {
+      const { data } = await axios.get(
+        `https://opentdb.com/api.php?amount=10${
+          category && `&category=${category}`
+        }${difficulty && `&difficulty=${difficulty}`}&type=multiple`
+      );
+
+      setQuestions(data.results);
+    } catch (error) {
+      if (error.response && error.response.status === 429) {
+        // Handle rate limit exceeded error
+        console.error("Rate limit exceeded. Please try again later.");
+      } else {
+        console.error("Error fetching questions:", error.message);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app" style={{ backgroundImage: "url(./ques1.png" }}>
+        <Header />
+        <Switch>
+          <Route path="/quiz">
+            <Quiz
+              name={name}
+              questions={questions}
+              score={score}
+              setScore={setScore}
+              setQuestions={setQuestions}
+            />
+          </Route>
+          <Route path="/result">
+            <Result name={name} score={score} />
+          </Route>
+          <Route path="/">
+            <Home
+              name={name}
+              setName={setName}
+              fetchQuestions={fetchQuestions}
+            />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div style ={{textAlign:'center',marginBottom:10}}>
+      Made with <i class="fas fa-loveseat    "></i>
+    </div>
+  )
+}
+
+export default Footer
